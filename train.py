1ef464537bab011b6477dd7ee3e5e4fd6d33162a
nickmaina1 <nick.maina1@gmail.com>
2017-10-31 21:44:26 +0300

    Finalize Model

--- train.py
+++ train.py
@@ -1,6 +1,85 @@
+import os
+
 import numpy as np
 import pandas as pd
-from keras.layers import LSTM, Bidirectional, Dense
-from keras.models import Sequential, load_model
-from keras.preprocessing.sequence import pad_sequences
-from keras.preprocessing.text import Tokenizer
+from keras.layers import (AveragePooling1D, Conv1D, Dense, Dropout, Embedding,
+                          GlobalMaxPooling1D, MaxPool1D)
+from keras.models import Sequential
+
+from utils import load_embeddings, load_processed_dataset
+
+TRAIN_FILE_TITLE = 'YELP REVIEWS POLARITY DATASET'
+TRAIN_FILE = 'datasets/yelp/train.csv'
+TEST_FILE = 'datasets/yelp/test.csv'
+
+POLARITY_LABEL = {'negative': 1, 'positive': 2}
+
+MAX_SEQUENCE = 1000
+MAX_WORDS = 20000
+
+def gen_model():
+    """
+    sequences: Embedding Sequences
+    """
+    sequences = embedding_layer()
+
+    model = Sequential([
+    sequences,
+    Conv1D(512, 5, activation='relu'),
+    AveragePooling1D(pool_size=5),
+    Conv1D(256, 5, activation='relu'),
+    AveragePooling1D(pool_size=5),
+    Conv1D(128, 5, activation='relu'),
+    MaxPool1D(pool_size=5),
+    GlobalMaxPooling1D(),
+    Dropout(0.3),
+    Dense(128, activation='relu'),
+    Dense(len(POLARITY_LABEL), activation='softmax')
+    ])
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['acc'])
+    
+    return model
+
+
+def embedding_layer():
+    EMBEDDINGS, EMBEDDING_DIM = load_embeddings()
+    matrix = np.zeros((MAX_WORDS, EMBEDDING_DIM)) # Embedding Matrix
+    max_words = min(MAX_WORDS, len(word_index))
+
+    for word, i in word_index.items():
+        if i >=MAX_WORDS:
+            continue
+        vector = EMBEDDINGS.get(word, None)
+        if vector is not None:
+            matrix[i]=vector
+        
+    layer = Embedding(MAX_WORDS,
+                    EMBEDDING_DIM, 
+                    weights=[matrix],
+                    input_length=MAX_SEQUENCE,
+                    trainable=False
+                    )
+    
+    return layer
+
+if __name__ == "__main__":
+    """
+    Ignore TensorFlow Warnings:
+      TF library wasn't compiled to use SSE instructions,..,but could speed up CPU computations
+    """
+    os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+    nrows = 5000
+    X, Y, word_index = load_processed_dataset(TRAIN_FILE, maxword=MAX_WORDS, rows=nrows, tokenize=True)
+    
+    split = nrows-int((nrows/10))
+    
+    X_train = X[:split]
+    Y_train = Y[:split]
+    
+    X_test =  X[split:]
+    Y_test = Y[split:]
+
+    
+    model = gen_model()
+    model.fit(X_train, Y_train, validation_data=[X_test, Y_test], batch_size=256, epochs=10)
+    model.save("sentiment.h5")
--- utils.py
+++ utils.py
@@ -1,2 +1,77 @@
-def load_dataset():
-	pass
+import pickle
+import numpy as np
+import pandas as pd
+
+
+def load_dataset(file, rows=None):
+    if not file:
+        return None, None
+    
+    """
+    Return Pandas DataFrame with the specified number of rows
+    """
+    r = rows or 20
+    
+    dataset = pd.read_csv(file, header=None, names=['sentiment', 'text'], nrows=r)
+    
+    X = dataset['text']
+    Y = dataset['sentiment']
+
+    Y = Y.replace(1, 0)
+    Y = Y.replace(2, 1)
+
+    return X.tolist(), Y.tolist()
+
+
+def load_processed_dataset(file=None, maxseq=None, maxword=None, **kwargs):
+    from keras.preprocessing.text import Tokenizer
+    from keras.preprocessing.sequence import pad_sequences
+    from keras.utils.np_utils import to_categorical
+
+    MAX_SEQUENCE = maxseq or 1000
+    MAX_WORDS = maxword or 20000
+
+    if not file:
+        return None, None
+
+    rows = kwargs.get('rows', None)
+    X_train, Y_train = load_dataset(file, rows)
+        
+    tokenizer = Tokenizer(num_words=MAX_WORDS)
+    tokenizer.fit_on_texts(X_train)
+    
+    word_index = tokenizer.word_index
+    sequences = tokenizer.texts_to_sequences(X_train)
+    
+    X1_train = pad_sequences(sequences, maxlen=MAX_SEQUENCE)
+
+    Y1_train = to_categorical(np.asarray(Y_train), 2)
+
+    del X_train
+    del Y_train
+    
+    return X1_train, Y1_train, word_index
+
+def load_embeddings(dim=True, embeddings=True) ->dict:
+    """
+    Returns:
+      If dimension and embeddings are true it returns the
+        pickled dict object and the dimensions
+
+      If embeddings is true and dim false returns embeddings only
+
+      If embeddings is false and dim true returns dim only
+
+    Example:
+      embeddings = load_embedding(dim=False)
+      dim = load_embedding(embeddings=False)
+
+      embeddings[<word>] Returns the coef of the word
+    """
+    if dim and embeddings:
+        return pickle.load(open('glove/glove.twitter.27B.25d.dict.p', 'rb')), 25
+    if dim and not embeddings:
+        return 25
+    if not dim and embeddings:
+        return pickle.load(open('glove/glove.twitter.27B.25d.dict.p', 'rb'))
+    return None, None
